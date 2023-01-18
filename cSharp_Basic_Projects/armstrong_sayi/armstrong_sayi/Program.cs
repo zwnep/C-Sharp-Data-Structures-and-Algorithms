4abcfc764c9b8f20a4bab6666c8392980bd1474a
zeynep demirtaş <71128703+zwnep@users.noreply.github.com>
2023-01-19 00:40:53 +0300

    Add files via upload

---
+++ cSharp_Basic_Projects/armstrong_sayi/armstrong_sayi/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace armstrong_sayi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("sayı girin: ");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+
+            int toplam = 0;
+            int gecici = sayi;
+            int basamakDeğeri = 0;
+
+            while (gecici > 0)
+            {
+                basamakDeğeri = gecici % 10;
+                toplam = basamakDeğeri + toplam;
+                gecici = gecici / 10;
+            }
+
+            if (sayi == toplam) Console.WriteLine("girilen sayının basamak sayılarını toplamı kendisine eşittir.");
+
+            else Console.WriteLine("sayı armstrong değildir.");
+        }
+    }
+}
